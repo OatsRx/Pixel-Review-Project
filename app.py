bdf8c656b868529ab9e7618479678e70d0e31806
OatsRx <otis.redman1992@gmail.com>
2020-09-13 21:33:20 +0000

    turned off debugging

--- app.py
+++ app.py
@@ -142,6 +142,6 @@ def delete_confirm():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=False)
