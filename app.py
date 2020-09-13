a9a839758b822a317d20ec5e1a531baf1cdeec49
OatsRx <otis.redman1992@gmail.com>
2020-09-13 11:26:47 +0000

    added imports to app.py, set up config and initial root. Env.py and requirements installed

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists('env.py'):
+    import env
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'pixelReviews'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+
+# Home
+@app.route('/')
+def home():
+    return render_template('home.html', page_title='Home')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
